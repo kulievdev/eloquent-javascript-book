5eb84c7d901b3b2dab8388ac4b87622acef11839
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-24 20:32:00 -0500

    work on chapter 4 problems

--- chapter-4/reverseArray.js
+++ chapter-4/reverseArray.js
@@ -1,4 +1,4 @@
-function reverseArray(array) {
+function reverseArray1(array) {
     const reversedArray = [];
 
     for (let i = array.length - 1; i >= 0; i--) {
@@ -8,9 +8,9 @@ function reverseArray(array) {
     return reversedArray;
 }
 
-console.log(reverseArray([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
+console.log(reverseArray1([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
 
-function reverseArrayInPlace(array) {
+function reverseArrayInPlace1(array) {
     for (let i = 0; i < array.length / 2; i++) {
         let temp = array[i];
         const lastEleIdx = array.length - 1 - i;
@@ -20,4 +20,31 @@ function reverseArrayInPlace(array) {
     return array;
 }
 
-console.log(reverseArrayInPlace([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
+console.log(reverseArrayInPlace1([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
+
+// Book solution
+
+function reverseArray(array) {
+    let output = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        output.push(array[i]);
+    }
+    return output;
+}
+
+function reverseArrayInPlace(array) {
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        let old = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
+    }
+    return array;
+}
+
+console.log(reverseArray(["A", "B", "C"])); //  ["C", "B", "A"];
+
+let arrayValue = [1, 2, 3, 4, 5];
+
+reverseArrayInPlace(arrayValue);
+
+console.log(arrayValue); //  [5, 4, 3, 2, 1]
